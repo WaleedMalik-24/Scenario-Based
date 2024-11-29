57a34cf3df94edde6276535d2d98eef20dbf8452
WaleedMalik-24 <waleedmalik1252@gmail.com>
2024-11-29 20:49:13 +0500

    id check.cpp

--- id check.cpp
+++ id check.cpp
@@ -3,42 +3,21 @@
 using namespace std;
 
 int main() {
-    string userId, password;
-    cout << "Enter your ID: ";
-    cin >> userId;
-    switch (0) {
-    case 0:
-   if (userId == "user1") 
-	{
-  cout << "Enter your password: ";
-  cin >> password;
-  switch (0) {
-  case 0:
-  if (password == "password123") 
-  {
-  cout << "Welcome, user1!" << endl;
-  } else {
-  cout << "Incorrect Password" << endl;
-  }
-  break;
-  }
-  } else if (userId == "user2") 
-  {
-  cout << "Enter your password: ";
-  cin >> password;
-  switch (0) {
-  case 0:
-  if (password == "securepass") 
-  {
-  cout << "Welcome, user2!" << endl;
-  } else {
-   cout << "Incorrect Password" << endl;
-  }
-   break;
-  }              
-  cout << "Incorrect ID" << endl;
-  }
-  }
-     return 0;
-}
+    string password = "1234", input;
+    int attempts = 3;
 
+    for (int i = 0; i < attempts; i++) {
+        cout << "Enter password: ";
+        cin >> input;
+
+        if (input == password) {
+            cout << "Access granted.\n";
+            return 0;
+        } else {
+            cout << "Incorrect password. " << (attempts - i - 1) << " attempts remaining.\n";
+        }
+    }
+
+    cout << "Access denied.\n";
+    return 0;
+}
