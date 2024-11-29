a127320ac26878cc3483956f3242ca5c3b30af3f
WaleedMalik-24 <waleedmalik1252@gmail.com>
2024-11-29 20:53:11 +0500

    reverse number pattern.cpp

--- reverse number pattern.cpp
+++ reverse number pattern.cpp
@@ -5,16 +5,13 @@ int main() {
     int n;
     cout << "Enter a positive integer: ";
     cin >> n;
-    if (n <= 0) {
-        cout << "Please enter a positive integer." << endl;
-    } else {
-        for (int i = n; i >= 1; --i) { // Outer loop for rows
-        for (int j = i; j >= 1; --j) { // Inner loop for columns
-            cout << j << " "; // Print the numbers in the row
-            }
-            cout << endl; // Move to the next line after each row
-        }
+
+    for (int i = n; i >= 1; i--) {
+        for (int j = i; j >= 1; j--)
+            cout << j << " ";
+        cout << endl;
     }
+
     return 0;
 }
 
