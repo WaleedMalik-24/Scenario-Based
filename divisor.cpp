fe1b9b35d17c56c04a2b112e71f92841176669e7
WaleedMalik-24 <waleedmalik1252@gmail.com>
2024-11-29 20:51:20 +0500

    divisor.cpp

--- divisor.cpp
+++ divisor.cpp
@@ -1,21 +1,18 @@
- #include <iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
-    int n; 
-   cout << "Enter a positive integer: ";
-    cin >> n;
-    if (n <= 0) {
-        cout << "Please enter a positive integer." << endl;
-    } else {
-        cout << "The divisors of " << n << " are: ";
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                cout << i << " "; 
-            }
-        }
-        cout << endl; 
+    int number;
+    cout << "Enter a positive integer: ";
+    cin >> number;
+
+    cout << "Divisors of " << number << ": ";
+    for (int i = 1; i <= number; i++) {
+        if (number % i == 0)
+            cout << i << " ";
     }
+    cout << endl;
+
     return 0;
 }
 
